1bdaa776c02ffebe579e34c41ce9c6fefcb62254
Jeffrey Wallace <jeffrey.wallace@live.com>
2013-05-30 20:48:25 -0700

    Did all the testing

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,12 +7,22 @@ class User < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me,
-  :first_name, :last_name, :profile_name
+                  :first_name, :last_name, :profile_name
   # attr_accessible :title, :body
 
-  	has_many :statuses
+  validates :first_name, presence: true
+
+  validates :last_name, presence: true
+
+  validates :profile_name, format: {
+    with: /\A[a-zA-Z\-\_]+\Z/,
+    message: "must be formatted correctly."
+  }
+
+  has_many :statuses
 
   def full_name
-  	first_name + " " + last_name
+    first_name + " " + last_name
   end
 end
+
